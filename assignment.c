aca8feb0e43f7d9cab95c02f48c78bf6e3ff2999
Honey780 <dabassohoney78@gmail.com>
2024-05-23 09:28:58 +0300

    first commit

---
+++ assignment.c
@@ -0,0 +1,16 @@
+//progrmm to add two numbers
+/*
+program to add two numbers
+Author: Honey
+Date: 16/05/2024
+*/
+#include <stdio.h> //scanf(), printf
+int main () {
+    int a = 50;
+    int b = 50;
+    int sum;
+    sum = a + b;
+    printf("The sum of a and b is %d",sum);
+    return 0;
+    
+}
